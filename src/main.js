28d18cef54dd7df58450c50267b6753ecb70a24b
MykytaKononov <nikitamaxkononov@gmail.com>
2025-02-02 19:26:21 +0100

    DONE2

--- src/main.js
+++ src/main.js
@@ -11,100 +11,55 @@ const gallery = document.getElementById('gallery');
 const pagebutton = document.querySelector('.pagebutton');
 let previousSearchRequest = '';
 
-document
-  .getElementById('form')
-  .addEventListener('submit', async function (event) {
-    event.preventDefault();
+async function handleSearch(event) {
+  event.preventDefault();
 
-    const searchrequest = document.getElementById('input').value.trim();
+  const searchrequest = document.getElementById('input').value.trim();
 
-    if (!searchrequest) {
-      iziToast.error({
-        title: 'Error',
-        message: 'Please enter search request!',
-        position: 'topRight',
-      });
-      return;
-    }
+  if (!searchrequest) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter search request!',
+      position: 'topRight',
+    });
+    return;
+  }
 
-    if (previousSearchRequest !== searchrequest) {
-      resetPage();
-      previousSearchRequest = searchrequest;
+  if (previousSearchRequest !== searchrequest) {
+    resetPage();
+    previousSearchRequest = searchrequest;
+  } else {
+    plusPage();
+  }
+  const loader = document.querySelector('.loader');
+  loader.style.visibility = 'visible';
+
+  try {
+    const images = await searchImages(searchrequest);
+    displayImages(images);
+    new SimpleLightbox('.image-link', {
+      captionsData: 'alt',
+      captionDelay: 500,
+    }).refresh();
+  } catch (error) {
+    console.error('RENDER ERROR', error);
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to load images. Please try again!',
+      position: 'topRight',
+    });
+  } finally {
+    loader.style.visibility = 'hidden';
+    if (gallery.children.length > 0) {
+      pagebutton.style.visibility = 'visible';
     } else {
-      plusPage();
+      pagebutton.style.visibility = 'hidden';
     }
-    const loader = document.querySelector('.loader');
-    loader.style.visibility = 'visible';
+  }
+}
 
-    try {
-      gallery.innerHTML = '';
-      const images = await searchImages(searchrequest);
-      displayImages(images);
-      new SimpleLightbox('.image-link', {
-        captionsData: 'alt',
-        captionDelay: 500,
-      }).refresh();
-    } catch (error) {
-      console.error('RENDER ERROR', error);
-      iziToast.error({
-        title: 'Error',
-        message: 'Failed to load images. Please try again!',
-        position: 'topRight',
-      });
-    } finally {
-      loader.style.visibility = 'hidden';
-      if (gallery.children.length > 0) {
-        pagebutton.style.visibility = 'visible';
-      } else {
-        pagebutton.style.visibility = 'hidden';
-      }
-    }
-  });
-document
-  .getElementById('page-button')
-  .addEventListener('click', async function (event) {
-    event.preventDefault();
-
-    const searchrequest = document.getElementById('input').value.trim();
-
-    if (!searchrequest) {
-      iziToast.error({
-        title: 'Error',
-        message: 'Please enter search request!',
-        position: 'topRight',
-      });
-      return;
-    }
-
-    if (previousSearchRequest !== searchrequest) {
-      resetPage();
-      previousSearchRequest = searchrequest;
-    } else {
-      plusPage();
-    }
-    const loader = document.querySelector('.loader');
-    loader.style.visibility = 'visible';
-
-    try {
-      const images = await searchImages(searchrequest);
-      displayImages(images);
-      new SimpleLightbox('.image-link', {
-        captionsData: 'alt',
-        captionDelay: 500,
-      }).refresh();
-    } catch (error) {
-      console.error('RENDER ERROR', error);
-      iziToast.error({
-        title: 'Error',
-        message: 'Failed to load images. Please try again!',
-        position: 'topRight',
-      });
-    } finally {
-      loader.style.visibility = 'hidden';
-      if (gallery.children.length > 0) {
-        pagebutton.style.visibility = 'visible';
-      } else {
-        pagebutton.style.visibility = 'hidden';
-      }
-    }
-  });
+document.getElementById('form').addEventListener('submit', event => {
+  gallery.innerHTML = '';
+  handleSearch(event);
+});
+document.getElementById('page-button').addEventListener('click', handleSearch);
